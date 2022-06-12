15dbcf21d616b51efa7d06f1163cb52cc7a62021
JJ Kim <jj@haedosa.xyz>
2022-06-12 16:32:09 +0900

    nixpkgs and home-manager changed to 22.05

--- flake.nix
+++ flake.nix
@@ -1,10 +1,10 @@
 {
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-21.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.05";
     flake-utils.url = "github:numtide/flake-utils/master";
     home-manager = {
-      url = "github:nix-community/home-manager/release-21.11";
+      url = "github:nix-community/home-manager/release-22.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };

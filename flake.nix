0cd3389cc919782dc81d289a240a35b9c487cdc0
JJ Kim <jj@haeodsa.xyz>
2022-10-01 13:07:22 +0900

    remove flake-uptils

--- flake.nix
+++ flake.nix
@@ -1,8 +1,8 @@
 {
 
   inputs = {
+
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils/master";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -13,6 +13,7 @@
       url = "github:nix-community/home-manager/release-22.05";
       inputs.nixpkgs.follows = "nixpkgs-stable";
     };
+
   };
 
   outputs = inputs: {

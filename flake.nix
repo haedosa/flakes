002c3392f421f7efa32bda8cc090b91dbd534017
JJ Kim <jj@haeodsa.xyz>
2022-10-01 13:04:43 +0900

    update flake inputs

--- flake.nix
+++ flake.nix
@@ -1,18 +1,20 @@
 {
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils/master";
+
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.05";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-22.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-22.05";
-    home-manager-stable = {
-      url = "github:nix-community/home-manager/release-22.05";
-      inputs.nixpkgs.follows = "nixpkgs-stable";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager-unstable = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
+
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = inputs: {

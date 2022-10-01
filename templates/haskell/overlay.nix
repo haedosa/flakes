63cb2861a73ca7fa088421d77ec79f3c480a105c
JJ Kim <jj@haeodsa.xyz>
2022-10-01 12:59:24 +0900

    remove cowsay and cowthink

--- templates/haskell/overlay.nix
+++ templates/haskell/overlay.nix
@@ -1,13 +1,5 @@
 final: prev: {
 
-  cowsay-who = final.writeShellScriptBin "cowsay-who" ''
-                 ${final.cowsay}/bin/cowsay "Hi $(${final.coreutils}/bin/whoami)"
-               '';
-
-  cowthink-where = final.writeShellScriptBin "cowthink-where" ''
-                 ${final.cowsay}/bin/cowthink "Am I at $(${final.nettools}/bin/hostname)?"
-               '';
-
   haskellPackages = prev.haskellPackages.override (old: {
     overrides = final.lib.composeManyExtensions [
       (old.overrides or  (_ : _: {}))

4f3a1160fc604e73b9abfd68b900daa134ebdac6
JJ Kim <jj@haeodsa.xyz>
2022-10-01 13:46:39 +0900

    revise develop.nix and add ghcid-exe

--- templates/haskell/develop.nix
+++ templates/haskell/develop.nix
@@ -1,12 +1,33 @@
-{ pkgs }: with pkgs;
-let
+{ pkgs }: with pkgs; let
 
-  ghcCharged = haskellPackages.ghcWithHoogle (p: with p; [
-    haskell-language-server
-    ghcid
-  ]);
+  ghcid-bin = haskellPackages.ghcid.bin;
 
-in
-pkgs.mkShell {
-  buildInputs = [ cabal-install ghcCharged ];
+  mk-ghcid-command = { name, target}:
+  runCommand name { buildInputs = [ makeWrapper ]; } ''
+    makeWrapper "${ghcid-bin}/bin/ghcid" \
+                $out/bin/${name} \
+                --add-flags \
+                "--command='cabal repl ${target}' \
+                --test 'Main.main'"
+  '';
+
+  ghcid-exe = mk-ghcid-command { name = "ghcid-exe"; target = "exe:haedosa"; };
+
+in mkShell {
+  buildInputs =
+    (with haskellPackages;
+
+    # build dependencies
+    haedosa.env.buildInputs ++
+    haedosa.env.nativeBuildInputs ++
+
+    # haskell tools
+    [ haskell-language-server
+      ghcid
+    ]) ++
+
+    # general tools
+    [ cabal-install
+      ghcid-exe
+    ];
 }

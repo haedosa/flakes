5bffd7922297950c6885ab6b1981ca445ae90ef5
JJ Kim <jj@haeodsa.xyz>
2022-12-19 12:52:55 +0900

    add inputs for 22.11 and update flakes

--- flake.nix
+++ flake.nix
@@ -8,6 +8,12 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    nixpkgs-22_11.url = "github:nixos/nixpkgs/nixos-22.11";
+    home-manager-22_11 = {
+      url = "github:nix-community/home-manager/release-22.11";
+      inputs.nixpkgs.follows = "nixpkgs-22_11";
+    };
+
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-22.05";
     home-manager-stable = {
       url = "github:nix-community/home-manager/release-22.05";

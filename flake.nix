11976039f890b2a7aeddd867c2e5064577028baf
JJ Kim <jj@haedosa.xyz>
2022-12-31 13:39:04 +0900

    make 22.11 stable

--- flake.nix
+++ flake.nix
@@ -8,15 +8,9 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    nixpkgs-22_11.url = "github:nixos/nixpkgs/nixos-22.11";
-    home-manager-22_11 = {
-      url = "github:nix-community/home-manager/release-22.11";
-      inputs.nixpkgs.follows = "nixpkgs-22_11";
-    };
-
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-22.05";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-22.11";
     home-manager-stable = {
-      url = "github:nix-community/home-manager/release-22.05";
+      url = "github:nix-community/home-manager/release-22.11";
       inputs.nixpkgs.follows = "nixpkgs-stable";
     };
 

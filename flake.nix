58c18b4d008cbaabe4e2b69ec31bd0db7fe655e8
JJ Kim <jj@haedosa.xyz>
2023-08-28 22:07:51 +0900

    update flake inputs

--- flake.nix
+++ flake.nix
@@ -2,16 +2,34 @@
 
   inputs = {
 
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
     home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:nix-community/home-manager/release-23.05";
+      inputs.nixpkgs.follows = "nixpkgs-23-05";
     };
 
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-22.11";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager-unstable = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
+    };
+
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-23.05";
     home-manager-stable = {
+      url = "github:nix-community/home-manager/release-23.05";
+      inputs.nixpkgs.follows = "nixpkgs-23-05";
+    };
+
+    nixpkgs-23-05.url = "github:nixos/nixpkgs/nixos-23.05";
+    home-manager-23-05 = {
+      url = "github:nix-community/home-manager/release-23.05";
+      inputs.nixpkgs.follows = "nixpkgs-23-05";
+    };
+
+    nixpkgs-22-11.url = "github:nixos/nixpkgs/nixos-22.11";
+    home-manager-22-11 = {
       url = "github:nix-community/home-manager/release-22.11";
-      inputs.nixpkgs.follows = "nixpkgs-stable";
+      inputs.nixpkgs.follows = "nixpkgs-22-11";
     };
 
   };

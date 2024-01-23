00eb9e998cf9d941a63e754f911227be7e91b0c8
JJ Kim <jj@haedosa.xyz>
2024-01-23 22:10:09 +0900

    update flake inputs

--- flake.nix
+++ flake.nix
@@ -8,28 +8,28 @@
       inputs.nixpkgs.follows = "nixpkgs-23-05";
     };
 
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager-unstable = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs-unstable";
-    };
-
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-23.05";
     home-manager-stable = {
       url = "github:nix-community/home-manager/release-23.05";
       inputs.nixpkgs.follows = "nixpkgs-23-05";
     };
 
-    nixpkgs-23-05.url = "github:nixos/nixpkgs/nixos-23.05";
-    home-manager-23-05 = {
-      url = "github:nix-community/home-manager/release-23.05";
-      inputs.nixpkgs.follows = "nixpkgs-23-05";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager-unstable = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
 
     nixpkgs-22-11.url = "github:nixos/nixpkgs/nixos-22.11";
     home-manager-22-11 = {
       url = "github:nix-community/home-manager/release-22.11";
       inputs.nixpkgs.follows = "nixpkgs-22-11";
+    };
+
+    nixpkgs-23-05.url = "github:nixos/nixpkgs/nixos-23.05";
+    home-manager-23-05 = {
+      url = "github:nix-community/home-manager/release-23.05";
+      inputs.nixpkgs.follows = "nixpkgs-23-05";
     };
 
     nixpkgs-23-11.url = "github:nixos/nixpkgs/nixos-23.11";
@@ -40,7 +40,21 @@
 
   };
 
-  outputs = inputs: {
+  outputs = inputs:
+  let
+    inherit (inputs.nixpkgs.lib) genAttrs;
+    supportedSystems = [ "x86_64-linux" ];
+    forAllSystems = genAttrs supportedSystems;
+    pkgsFor = nixpkgs_: system: import nixpkgs_ { inherit system; config = { allowUnfree = true; }; };
+  in
+  {
+
+    pkgs = forAllSystems (system: pkgsFor inputs.nixpkgs system);
+    pkgs-stable = forAllSystems (system: pkgsFor inputs.nixpkgs-stable system);
+    pkgs-unstable = forAllSystems (system: pkgsFor inputs.nixpkgs-unstable system);
+    pkgs-22-11 = forAllSystems (system: pkgsFor inputs.nixpkgs-22-11 system);
+    pkgs-23-05 = forAllSystems (system: pkgsFor inputs.nixpkgs-23-05 system);
+    pkgs-23-11 = forAllSystems (system: pkgsFor inputs.nixpkgs-23-11 system);
 
     templates = {
 

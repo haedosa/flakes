d632f3902361d3d6f7b433b2d949319116e7e5ff
JJ Kim <jj@haedosa.xyz>
2024-04-29 00:03:23 +0900

    simplify nixpkgs dep

--- flake.nix
+++ flake.nix
@@ -2,40 +2,10 @@
 
   inputs = {
 
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.05";
-      inputs.nixpkgs.follows = "nixpkgs-23-05";
-    };
-
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-23.05";
-    home-manager-stable = {
-      url = "github:nix-community/home-manager/release-23.05";
-      inputs.nixpkgs.follows = "nixpkgs-23-05";
-    };
-
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager-unstable = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs-unstable";
-    };
-
-    nixpkgs-22-11.url = "github:nixos/nixpkgs/nixos-22.11";
-    home-manager-22-11 = {
-      url = "github:nix-community/home-manager/release-22.11";
-      inputs.nixpkgs.follows = "nixpkgs-22-11";
-    };
-
-    nixpkgs-23-05.url = "github:nixos/nixpkgs/nixos-23.05";
-    home-manager-23-05 = {
-      url = "github:nix-community/home-manager/release-23.05";
-      inputs.nixpkgs.follows = "nixpkgs-23-05";
-    };
-
-    nixpkgs-23-11.url = "github:nixos/nixpkgs/nixos-23.11";
-    home-manager-23-11 = {
       url = "github:nix-community/home-manager/release-23.11";
-      inputs.nixpkgs.follows = "nixpkgs-23-11";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
 
   };
@@ -50,11 +20,6 @@
   {
 
     pkgs = forAllSystems (system: pkgsFor inputs.nixpkgs system);
-    pkgs-stable = forAllSystems (system: pkgsFor inputs.nixpkgs-stable system);
-    pkgs-unstable = forAllSystems (system: pkgsFor inputs.nixpkgs-unstable system);
-    pkgs-22-11 = forAllSystems (system: pkgsFor inputs.nixpkgs-22-11 system);
-    pkgs-23-05 = forAllSystems (system: pkgsFor inputs.nixpkgs-23-05 system);
-    pkgs-23-11 = forAllSystems (system: pkgsFor inputs.nixpkgs-23-11 system);
 
     templates = {
 
